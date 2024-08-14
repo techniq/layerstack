4f24d1ff9bd097d95e397114a141e05488896c1d
Sean Lynch <techniq35@gmail.com>
2024-08-13 23:45:30 -0400

    Remove vitest from sites/docs `vite.config.js`

--- sites/docs/vite.config.js
+++ sites/docs/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { sveld } from './src/lib/plugins/vite.js';
 
 export default defineConfig({
   plugins: [sveltekit(), sveld()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-    deps: {
-      optimizer: {
-        ssr: {
-          exclude: ['sveld'],
-        },
-      },
-    },
-    coverage: {
-      reporter: ['html'],
-    },
-  },
 });
