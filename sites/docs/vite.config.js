52961fc0abd67bd18ae9295b3adb229ccad4d296
Sean Lynch <techniq35@gmail.com>
2025-03-13 15:10:31 -0400

    Switch from `@tailwindcss/postcss` to `@tailwindcss/vite`

--- sites/docs/vite.config.js
+++ sites/docs/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { sveld } from './src/lib/plugins/vite.js';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), sveld()],
+  plugins: [tailwindcss(), sveltekit(), sveld()],
 });
