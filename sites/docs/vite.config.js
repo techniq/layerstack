184af284098c8abdd3a150dca61604fd4f372a09
Sean Lynch <techniq35@gmail.com>
2025-05-14 11:35:39 -0400

    Update `@sveltejs/adapter-cloudflare`

--- sites/docs/vite.config.js
+++ sites/docs/vite.config.js
@@ -4,4 +4,7 @@ import { sveld } from './src/lib/plugins/vite.js';
 
 export default defineConfig({
   plugins: [sveltekit(), sveld()],
+  resolve: {
+    noExternal: true, // https://github.com/AdrianGonz97/refined-cf-pages-action/issues/26#issuecomment-2878397440
+  },
 });
