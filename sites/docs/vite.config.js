e1e87503461ef64929158ad88b84978968404368
Sean Lynch <techniq35@gmail.com>
2025-05-14 12:22:14 -0400

    FIx lint

--- sites/docs/vite.config.js
+++ sites/docs/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { sveld } from './src/lib/plugins/vite.js';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()/*, sveld()*/],
+  plugins: [tailwindcss(), sveltekit() /*, sveld()*/],
   resolve: {
     noExternal: true, // https://github.com/AdrianGonz97/refined-cf-pages-action/issues/26#issuecomment-2878397440
   },
