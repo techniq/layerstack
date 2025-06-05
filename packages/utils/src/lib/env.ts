a6df412a42636fead1b1ce4f92d114ecc6b1d727
Sean Lynch <techniq35@gmail.com>
2025-06-05 11:08:00 -0400

    feat: Add `platform` to get details of browser (operating system, etc)

--- packages/utils/src/lib/env.ts
+++ packages/utils/src/lib/env.ts
@@ -2,3 +2,50 @@
 // See: https://kit.svelte.dev/docs/packaging
 export const browser = typeof window !== 'undefined';
 export const ssr = typeof window === 'undefined';
+
+/**
+ * Get platform details of browser (operating system, etc.)
+ */
+export const platform = {
+  getOS() {
+    const userAgent = navigator.userAgent;
+
+    if (userAgent.includes('Mac')) {
+      return 'macOS';
+    } else if (userAgent.includes('Windows')) {
+      return 'Windows';
+    } else if (userAgent.includes('Linux')) {
+      return 'Linux';
+    } else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
+      return 'iOS';
+    } else if (userAgent.includes('Android')) {
+      return 'Android';
+    }
+
+    return 'Unknown';
+  },
+
+  isMac() {
+    return this.getOS() === 'macOS';
+  },
+  isWindows() {
+    return this.getOS() === 'Windows';
+  },
+  isLinux() {
+    return this.getOS() === 'Linux';
+  },
+  isIOS() {
+    return this.getOS() === 'iOS';
+  },
+  isAndroid() {
+    return this.getOS() === 'Android';
+  },
+
+  getModifierSymbol() {
+    return this.isMac() ? '⌘' : 'Ctrl';
+  },
+
+  getModifierKey() {
+    return this.isMac() ? 'metaKey' : 'ctrlKey';
+  },
+};
