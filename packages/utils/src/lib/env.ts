e47ed359ae04ce24d70f3ab5369366f77cdc93d6
Sean Lynch <techniq35@gmail.com>
2025-06-05 11:35:41 -0400

    refactor: Remove `platform` nesting of user agent introspection functions (already nested in `env` export)

--- packages/utils/src/lib/env.ts
+++ packages/utils/src/lib/env.ts
@@ -4,48 +4,46 @@ export const browser = typeof window !== 'undefined';
 export const ssr = typeof window === 'undefined';
 
 /**
- * Get platform details of browser (operating system, etc.)
+ * Get the operating system of the browser
  */
-export const platform = {
-  getOS() {
-    const userAgent = navigator.userAgent;
+export function getOS() {
+  const userAgent = navigator.userAgent;
 
-    if (userAgent.includes('Mac')) {
-      return 'macOS';
-    } else if (userAgent.includes('Windows')) {
-      return 'Windows';
-    } else if (userAgent.includes('Linux')) {
-      return 'Linux';
-    } else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
-      return 'iOS';
-    } else if (userAgent.includes('Android')) {
-      return 'Android';
-    }
+  if (userAgent.includes('Mac')) {
+    return 'macOS';
+  } else if (userAgent.includes('Windows')) {
+    return 'Windows';
+  } else if (userAgent.includes('Linux')) {
+    return 'Linux';
+  } else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
+    return 'iOS';
+  } else if (userAgent.includes('Android')) {
+    return 'Android';
+  }
 
-    return 'Unknown';
-  },
+  return 'Unknown';
+}
 
-  isMac() {
-    return this.getOS() === 'macOS';
-  },
-  isWindows() {
-    return this.getOS() === 'Windows';
-  },
-  isLinux() {
-    return this.getOS() === 'Linux';
-  },
-  isIOS() {
-    return this.getOS() === 'iOS';
-  },
-  isAndroid() {
-    return this.getOS() === 'Android';
-  },
+export function isMac() {
+  return getOS() === 'macOS';
+}
+export function isWindows() {
+  return getOS() === 'Windows';
+}
+export function isLinux() {
+  return getOS() === 'Linux';
+}
+export function isIOS() {
+  return getOS() === 'iOS';
+}
+export function isAndroid() {
+  return getOS() === 'Android';
+}
 
-  getModifierSymbol() {
-    return this.isMac() ? '⌘' : 'Ctrl';
-  },
+export function getModifierSymbol() {
+  return isMac() ? '⌘' : 'Ctrl';
+}
 
-  getModifierKey() {
-    return this.isMac() ? 'metaKey' : 'ctrlKey';
-  },
-};
+export function getModifierKey() {
+  return isMac() ? 'metaKey' : 'ctrlKey';
+}
